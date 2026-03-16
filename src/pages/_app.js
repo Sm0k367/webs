826240e4a242526b0acf081f3d7e34062a10c609
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-03-15 22:21:38 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css';
+import { useState } from 'react';
+import QuickLogin from '../components/QuickLogin';
 
 export default function App({ Component, pageProps }) {
-return <Component {...pageProps} />;
+const [user, setUser] = useState(null);
+
+if (!user) {
+return (
+<div className="min-h-screen flex items-center justify-center bg-black">
+<QuickLogin onLogin={setUser} />
+</div>
+);
+}
+
+// Pass user to every page as prop
+return <Component {...pageProps} user={user} />;
 }
